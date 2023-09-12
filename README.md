8c136f6355c03bb403a42b628305e1d16a9c250e
0x4r2 <jose_docs@hotmail.com>
2023-09-12 16:54:53 -0500

    Update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CustomKali
 Convert kali to the best hacking machine
+
+
+## Using the Script
+
+Donwload the file and execute as admin user, no **root**!
+
+```bash
+wget https://raw.githubusercontent.com/0x4r2/CustomKali/main/customizeme.sh
+
+chmod +x customizeme.sh
+
+./customizeme.sh
+```
+
+
--- customizeme.sh
+++ customizeme.sh
@@ -107,182 +107,22 @@ wget https://raw.githubusercontent.com/0x4r2/CustomKali/main/Recursos/keyboard-l
 
 #________Set 2-4 Workspaces________
 
-xmlstarlet ed --inplace -u "//property[@name='workspace_count']/@value" -v "2"  /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-xmlstarlet ed --inplace -d "//property[@name='workspace_names']/value"  /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+#xmlstarlet ed --inplace -u "//property[@name='workspace_count']/@value" -v "2"  /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+#xmlstarlet ed --inplace -d "//property[@name='workspace_names']/value"  /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 
-xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "P"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "S"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "3"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "4"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+#xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "P"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+#xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "S"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+#xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "3"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+#xmlstarlet ed --inplace -s "//property[@name='workspace_names']" -t elem -n "value" -v "" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "type" -v "string" -i "//property[@name='workspace_names']/value[last()]" -t attr -n "value" -v "4"   /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 
 
 #_______Custom Panel Bar________
 
-wget  https://github.com/0x4r2/CustomKali/raw/main/Recursos/panel.zip -O /tmp/panel.zip
-unzip -o -d /home/$USERNAME/.config/xfce4/ /tmp/panel.zip
+#wget  https://github.com/0x4r2/CustomKali/raw/main/Recursos/panel.zip -O /tmp/panel.zip
+#unzip -o -d /home/$USERNAME/.config/xfce4/ /tmp/panel.zip
 
-cat << EOF >/home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml 
-<?xml version="1.0" encoding="UTF-8"?>
-
-<channel name="xfce4-panel" version="1.0">
-  <property name="configver" type="int" value="2"/>
-  <property name="panels" type="array">
-    <value type="int" value="1"/>
-    <property name="panel-1" type="empty">
-      <property name="position" type="string" value="p=6;x=0;y=0"/>
-      <property name="length" type="uint" value="100"/>
-      <property name="position-locked" type="bool" value="true"/>
-      <property name="size" type="uint" value="34"/>
-      <property name="plugin-ids" type="array">
-        <value type="int" value="1"/>
-        <value type="int" value="2"/>
-        <value type="int" value="3"/>
-        <value type="int" value="4"/>
-        <value type="int" value="5"/>
-        <value type="int" value="6"/>
-        <value type="int" value="7"/>
-        <value type="int" value="8"/>
-        <value type="int" value="9"/>
-        <value type="int" value="10"/>
-        <value type="int" value="11"/>
-        <value type="int" value="12"/>
-        <value type="int" value="24"/>
-        <value type="int" value="25"/>
-        <value type="int" value="23"/>
-        <value type="int" value="26"/>
-        <value type="int" value="18"/>
-        <value type="int" value="14"/>
-        <value type="int" value="15"/>
-        <value type="int" value="16"/>
-        <value type="int" value="17"/>
-        <value type="int" value="19"/>
-        <value type="int" value="20"/>
-        <value type="int" value="21"/>
-        <value type="int" value="22"/>
-        <value type="int" value="13"/>
-      </property>
-    </property>
-  </property>
-  <property name="plugins" type="empty">
-    <property name="plugin-1" type="string" value="whiskermenu"/>
-    <property name="plugin-2" type="string" value="separator"/>
-    <property name="plugin-3" type="string" value="showdesktop"/>
-    <property name="plugin-4" type="string" value="directorymenu">
-      <property name="icon-name" type="string" value="system-file-manager"/>
-      <property name="base-directory" type="string" value="/home/$USERNAME"/>
-    </property>
-    <property name="plugin-5" type="string" value="launcher">
-      <property name="items" type="array">
-        <value type="string" value="16944410411.desktop"/>
-      </property>
-    </property>
-    <property name="plugin-6" type="string" value="launcher">
-      <property name="items" type="array">
-        <value type="string" value="16944410412.desktop"/>
-        <value type="string" value="16944539461.desktop"/>
-      </property>
-    </property>
-    <property name="plugin-7" type="string" value="launcher">
-      <property name="items" type="array">
-        <value type="string" value="16944410413.desktop"/>
-        <value type="string" value="16944410414.desktop"/>
-      </property>
-      <property name="move-first" type="bool" value="true"/>
-    </property>
-    <property name="plugin-8" type="string" value="separator"/>
-    <property name="plugin-9" type="string" value="pager">
-      <property name="miniature-view" type="bool" value="false"/>
-      <property name="rows" type="uint" value="1"/>
-    </property>
-    <property name="plugin-900" type="string" value="pager">
-      <property name="miniature-view" type="bool" value="true"/>
-      <property name="rows" type="uint" value="2"/>
-    </property>
-    <property name="plugin-10" type="string" value="separator"/>
-    <property name="plugin-11" type="string" value="tasklist">
-      <property name="show-handle" type="bool" value="false"/>
-      <property name="show-labels" type="bool" value="false"/>
-      <property name="middle-click" type="uint" value="1"/>
-      <property name="grouping" type="uint" value="1"/>
-    </property>
-    <property name="plugin-12" type="string" value="separator">
-      <property name="expand" type="bool" value="true"/>
-      <property name="style" type="uint" value="0"/>
-    </property>
-    <property name="plugin-14" type="string" value="systray">
-      <property name="size-max" type="uint" value="22"/>
-      <property name="square-icons" type="bool" value="true"/>
-      <property name="symbolic-icons" type="bool" value="false"/>
-      <property name="known-legacy-items" type="array">
-        <value type="string" value="networkmanager applet"/>
-        <value type="string" value="ethernet network connection “wired connection 1” active"/>
-      </property>
-    </property>
-    <property name="plugin-15" type="string" value="genmon"/>
-    <property name="plugin-16" type="string" value="pulseaudio">
-      <property name="enable-keyboard-shortcuts" type="bool" value="true"/>
-    </property>
-    <property name="plugin-17" type="string" value="notification-plugin"/>
-    <property name="plugin-19" type="string" value="clock">
-      <property name="digital-layout" type="uint" value="3"/>
-      <property name="digital-time-format" type="string" value="%_H:%M"/>
-      <property name="digital-time-font" type="string" value="Cantarell 11"/>
-    </property>
-    <property name="plugin-20" type="string" value="separator">
-      <property name="style" type="uint" value="0"/>
-    </property>
-    <property name="plugin-21" type="string" value="separator"/>
-    <property name="plugin-22" type="string" value="actions">
-      <property name="appearance" type="uint" value="0"/>
-      <property name="items" type="array">
-        <value type="string" value="+lock-screen"/>
-        <value type="string" value="-switch-user"/>
-        <value type="string" value="-separator"/>
-        <value type="string" value="-suspend"/>
-        <value type="string" value="-hibernate"/>
-        <value type="string" value="-hybrid-sleep"/>
-        <value type="string" value="-separator"/>
-        <value type="string" value="-shutdown"/>
-        <value type="string" value="-restart"/>
-        <value type="string" value="-separator"/>
-        <value type="string" value="+logout"/>
-        <value type="string" value="-logout-dialog"/>
-      </property>
-    </property>
-    <property name="plugin-2200" type="string" value="actions">
-      <property name="appearance" type="uint" value="0"/>
-      <property name="items" type="array">
-        <value type="string" value="-lock-screen"/>
-        <value type="string" value="-switch-user"/>
-        <value type="string" value="-separator"/>
-        <value type="string" value="-suspend"/>
-        <value type="string" value="-hibernate"/>
-        <value type="string" value="-hybrid-sleep"/>
-        <value type="string" value="-separator"/>
-        <value type="string" value="-shutdown"/>
-        <value type="string" value="-restart"/>
-        <value type="string" value="-separator"/>
-        <value type="string" value="+logout"/>
-        <value type="string" value="-logout-dialog"/>
-      </property>
-    </property>
-    <property name="plugin-13" type="string" value="xkb">
-      <property name="display-type" type="uint" value="2"/>
-      <property name="display-name" type="uint" value="0"/>
-      <property name="caps-lock-indicator" type="bool" value="true"/>
-      <property name="show-notifications" type="bool" value="false"/>
-      <property name="display-tooltip-icon" type="bool" value="true"/>
-      <property name="group-policy" type="uint" value="0"/>
-    </property>
-    <property name="plugin-18" type="string" value="genmon"/>
-    <property name="plugin-23" type="string" value="genmon"/>
-    <property name="plugin-24" type="string" value="genmon"/>
-    <property name="plugin-25" type="string" value="separator"/>
-    <property name="plugin-26" type="string" value="separator"/>
-  </property>
-</channel>
-
-EOF
+wget  https://github.com/0x4r2/CustomKali/raw/main/Recursos/custom4r2.tar.bz2 -O /home/$USERNAME/.local/share/xfce4-panel-profiles/custom4r2.tar.bz2
+xfce4-panel-profiles load /home/$USERNAME/.local/share/xfce4-panel-profiles/custom4r2.tar.bz2
 
 
 
@@ -382,4 +222,8 @@ sudo cp -f /home/$USERNAME/.config/qterminal.org/qterminal.ini /etc/xdg/qtermina
 sudo cp -f /home/$USERNAME/.zshrc /root/.zshrc
 
 sudo rm -rf /tmp/*
+
+echo -e "\n\n[+] Instalación Completa !!\n"
+read -p "Presiona enter para reiniciar.... "
+
 reboot
